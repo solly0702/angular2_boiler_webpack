6eb921891961ee4832ec35f7b99a613e9d78a224
solly0702 <solly0702@gmail.com>
2016-11-17 03:38:50 -0800

    first commit for angular2-boiler-webpack

---
+++ src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+// import '../../public/css/styles.css';
+
+@Component({
+  selector: 'app',
+  template: '<h1>Hello Angular2</h1>',
+})
+
+export class AppComponent { }
